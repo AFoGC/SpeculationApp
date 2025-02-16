f2a369fa928df1f9ef11d04475a51a4afff3c942
AFoGC <sirkoandrew84@gmail.com>
2025-02-16 04:19:05 +0200

    Добавил миграции

---
+++ SpeculatorApp.Dal/Migrations/20250216021807_Initial.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpeculationApp.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Code = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OperationTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IsIncrease = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OperationTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pairs",
+                columns: table => new
+                {
+                    BaseCurrencyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TradeCurrencyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PositionInList = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pairs", x => new { x.BaseCurrencyId, x.TradeCurrencyId });
+                    table.ForeignKey(
+                        name: "FK_Pairs_Currencies_BaseCurrencyId",
+                        column: x => x.BaseCurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pairs_Currencies_TradeCurrencyId",
+                        column: x => x.TradeCurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Operations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OperationTypeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CurrencyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Amount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Operations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Operations_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Operations_OperationTypes_OperationTypeId",
+                        column: x => x.OperationTypeId,
+                        principalTable: "OperationTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Convertations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BaseCurrencyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TradeCurrencyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BaseCurrencyAmount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    TradeCurrencyAmount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ToTradeCurrency = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Convertations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Convertations_Pairs_BaseCurrencyId_TradeCurrencyId",
+                        columns: x => new { x.BaseCurrencyId, x.TradeCurrencyId },
+                        principalTable: "Pairs",
+                        principalColumns: new[] { "BaseCurrencyId", "TradeCurrencyId" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Convertations_BaseCurrencyId_TradeCurrencyId",
+                table: "Convertations",
+                columns: new[] { "BaseCurrencyId", "TradeCurrencyId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Operations_CurrencyId",
+                table: "Operations",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Operations_OperationTypeId",
+                table: "Operations",
+                column: "OperationTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pairs_TradeCurrencyId",
+                table: "Pairs",
+                column: "TradeCurrencyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Convertations");
+
+            migrationBuilder.DropTable(
+                name: "Operations");
+
+            migrationBuilder.DropTable(
+                name: "Pairs");
+
+            migrationBuilder.DropTable(
+                name: "OperationTypes");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+        }
+    }
+}
